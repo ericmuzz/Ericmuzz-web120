acc2efc07427901bbcdab4dcec881a97708cc5ec
ericmuzz <eric.muzzarelli@seattlecentral.edu>
2018-08-06 13:25:06 -0700

    adjust error in file management

--- web120/web120/contactme.php
+++
@@ -1,8 +0,0 @@
-<?php include "includes/header-fullwidth.php"?>
-
-
-<?php include "includes/simple.php";?>
-<p class="clear-recaptcha"></p>
-</section>
-
-<?php include "includes/footer.php"?>
--- web120/web120/fp/contact.php
+++
@@ -1,6 +0,0 @@
-<?php include "includes/header.php"?>
-<?php include 'includes/multiple.php';?>
-   </section>
-      <!-- end left column -->
-
-   <?php include "includes/footer.php"?>
